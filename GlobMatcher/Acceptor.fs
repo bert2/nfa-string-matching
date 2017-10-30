e75c62ac2590f27c504c7a4fbf7b3ae7dc96b855
bert2 <shuairan@gmail.com>
2017-10-30 14:44:35 +0100

    Refactor Acceptor and add example.

--- GlobMatcher/Acceptor.fs
+++ GlobMatcher/Acceptor.fs
@@ -1,24 +1,46 @@
 ﻿module Acceptor
 
-type State = Success | State of Transition list
-and Transition = { Target: State; Accepts: Word }
-and Word = Word of string | Anything
+type Word = Word of string | Anything
+type State = State | Success | Failure
+type Transition = {Start: State; End: State; Accepts: Word}
 
-let consume currentState word =
+let consume currentState transitions word =
+    let isOutgoingFrom state {Start = start} = state = start
+
     let accepts word transition =
         match word, transition with
         | Word x, {Accepts = Word y} when x = y -> true
         | Anything, {Accepts = Anything} -> true
         | _ -> false
 
-    let findAccepting word transitions = 
+    let findAccepting word transitions =
         match transitions |> List.tryFind (accepts word) with
         | None -> transitions |> List.tryFind (accepts Anything)
         | t -> t
 
     match currentState with
-    | Success -> currentState
-    | State transitions ->
-        match transitions |> findAccepting word with
-        | Some {Target = target} -> target
-        | None -> currentState
+    | Failure -> Failure
+    | Success -> Failure
+    | _ ->
+        let outgoing = transitions |> List.filter (isOutgoingFrom currentState)
+        match outgoing |> findAccepting word with
+        | Some {End = nextState} -> nextState
+        | None -> Failure
+
+let rec accept startState transitions (text:string) =
+    let getHead (str:string) =
+        match str.Length with
+        | 0 -> Word ""
+        | _ -> Word str.[..0]
+
+    let nextState = 
+        text
+        |> getHead
+        |> consume startState transitions
+
+    match text.Length with
+    | 0 -> nextState = Success
+    | _ -> 
+        let tail = text.[1..]
+        accept nextState transitions tail
+
--- GlobMatcher/Program.fs
+++ GlobMatcher/Program.fs
@@ -1,7 +1,25 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open Acceptor
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    // State machine for the pattern "a*c*e":
+    let one = State
+    let two = State
+    let three = State
+    let four = State
+    let five = Success
+
+    let a = {Start = one; End = two; Accepts = Word "a"}
+    let ``* (1)`` = {Start = two; End = two; Accepts = Anything}
+    let c = {Start = two; End = three; Accepts = Word "c"}
+    let ``* (2)`` = {Start = three; End = three; Accepts = Anything}
+    let e = {Start = three; End = four; Accepts = Word "e"}
+    let ``e again`` = {Start = four; End = four; Accepts = Word "e"}
+    let backtrack = {Start = four; End = three; Accepts = Anything}
+    let ``end of string`` = {Start = four; End = five; Accepts = Word ""}
+
+    let transistions = [a; ``* (1)``; c; ``* (2)``; e; ``e again``; backtrack; ``end of string``]
+
+    let result = accept one transistions argv.[0]
+    printfn "%A" result
     0 // return an integer exit code
