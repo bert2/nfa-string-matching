aad5403c58922040c89398b8dc5ae98c57b9a216
bert2 <shuairan@gmail.com>
2017-10-30 16:58:08 +0100

    Add parser for patterns with ? or multiple characters.

---
+++ GlobMatcher/Parser.fs
@@ -0,0 +1,45 @@
+﻿module Parser
+
+open System
+open System.Globalization
+open System.Text.RegularExpressions
+open Acceptor
+
+let private reverse s =
+    seq {
+        let e = StringInfo.GetTextElementEnumerator(s)
+        while e.MoveNext() do
+            yield e.GetTextElement()
+    }
+    |> Array.ofSeq
+    |> Array.rev
+    |> String.concat ""
+
+let private newShortId () =
+    let id = Guid.NewGuid().ToByteArray() |> Convert.ToBase64String
+    Regex.Replace(id, "[/+=]", "")
+
+let private parseWord (oldStart::states) transitions word =
+    match word with
+    | "*" -> oldStart::states, transitions
+    | "?" ->
+        let newStart = State (newShortId () |> Id)
+        let t = {Start = newStart; End = oldStart; Accepts = Anything}
+        newStart::oldStart::states, t::transitions
+    | _ -> 
+        let newStart = State (newShortId () |> Id)
+        let t = {Start = newStart; End = oldStart; Accepts = Word word}
+        newStart::oldStart::states, t::transitions
+    
+let rec private parse (start::states) transitions (pattern:string) =
+    match pattern.Length with
+    | 0 -> start, transitions
+    | _ -> 
+        let states', transistions' = parseWord (start::states) transitions pattern.[..0]
+        parse states' transistions' pattern.[1..]
+
+let toAcceptor pattern =
+    let initial = State (newShortId () |> Id)
+    let halt = Success
+    let ``end of input`` = {Start = initial; End = halt; Accepts = Word ""}
+    parse [initial; halt] [``end of input``] (reverse pattern)
--- GlobMatcher/Program.fs
+++ GlobMatcher/Program.fs
@@ -1,30 +1,47 @@
 ﻿open Acceptor
-open System
+open Parser
 
 [<EntryPoint>]
 let main argv = 
     // State machine for the pattern "a*c*e":
+    let start = State (Id "start")
     let one = State (Id "one")
     let two = State (Id "two")
     let three = State (Id "three")
-    let four = State (Id "four")
-    let five = Success
+    let stop = Success
 
-    let a = {Start = one; End = two; Accepts = Word "a"}
-    let ``* (1)`` = {Start = two; End = two; Accepts = Anything}
-    let c = {Start = two; End = three; Accepts = Word "c"}
-    let ``* (2)`` = {Start = three; End = three; Accepts = Anything}
-    let e = {Start = three; End = four; Accepts = Word "e"}
-    let ``e again`` = {Start = four; End = four; Accepts = Word "e"}
-    let backtrack = {Start = four; End = three; Accepts = Anything}
-    let ``end of string`` = {Start = four; End = five; Accepts = Word ""}
+    let a = {Start = start; End = one; Accepts = Word "a"}
+    let ``* (1)`` = {Start = one; End = one; Accepts = Anything}
+    let c = {Start = one; End = two; Accepts = Word "c"}
+    let ``* (2)`` = {Start = two; End = two; Accepts = Anything}
+    let e = {Start = two; End = three; Accepts = Word "e"}
+    let ``e again`` = {Start = three; End = three; Accepts = Word "e"}
+    let backtrack = {Start = three; End = two; Accepts = Anything}
+    let ``end of string`` = {Start = three; End = stop; Accepts = Word ""}
 
     let transitions = [a; ``* (1)``; c; ``* (2)``; e; ``e again``; backtrack; ``end of string``]
     let text =
         match argv.Length with
-        | 0 -> "acef"
+        | 0 -> "abcde"
         | _ -> argv.[0]
 
-    let result = accept one transitions text
+    let result = accept start transitions text
     printfn "%A" result
+
+    let testParser pattern text =
+        let s,ts = toAcceptor pattern
+        printfn "pattern: %A,\ttext: %A\t-> %A" pattern text (accept s ts text)
+    
+    testParser "" ""
+    testParser "" "a"
+    testParser "a" "a"
+    testParser "a" "b"
+    testParser "a" ""
+    testParser "ab" "ab"
+    testParser "ab" "abc"
+    testParser "?" ""
+    testParser "?" "a"
+    testParser "?" "b"
+    testParser "?" "ab"
+
     0 // return an integer exit code
