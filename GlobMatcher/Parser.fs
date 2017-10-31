a86e6243a0e60fb98c4697ee1ec6c85f2bbd2812
bert2 <shuairan@gmail.com>
2017-10-31 12:49:50 +0100

    Add backtracking for * wildcard.

--- GlobMatcher/Parser.fs
+++ GlobMatcher/Parser.fs
@@ -6,22 +6,32 @@ open Util
 let private findTransition startState endState =
     List.find (fun {Start = s; End = e} -> s = startState && e = endState)
 
+let private acceptsAnything state =
+    List.exists (fun {Start = s; Accepts = w} -> s = state && w = Anything)
+
 let private extentWithFixedLengthWord (oldStart::states) transitions word =
     let newStart = State (shortId () |> UniqueId)
     let t = {Start = newStart; End = oldStart; Accepts = word}
     newStart::oldStart::states, t::transitions
 
+let rec private extendWithBacktracking target firstSuccessor exitWord successors transitions =
+    match successors with
+    | [] -> transitions
+    | [Success] -> transitions
+    | state::successors' ->
+        if not (acceptsAnything state transitions) then
+            let backtrack = {Start = state; End = target; Accepts = Anything}
+            let hold = {Start = state; End = firstSuccessor; Accepts = exitWord}
+            backtrack::hold::(extendWithBacktracking target firstSuccessor exitWord successors' transitions)
+        else
+            // We reached another "*" -- backtracking can stop here.
+            transitions
+
 let private extentWithVariableLengthWord (fst::snd::states) transitions =
-    let t1 = {Start = fst; End = fst; Accepts = Anything}
-    let transitions' =
-        match snd with
-        | Success -> t1::transitions
-        | _ ->
-            let backtrack = {Start = snd; End = fst; Accepts = Anything}
-            let {Accepts = word} = transitions |> findTransition fst snd
-            let t3 = {Start = snd; End = snd; Accepts = word}
-            t1::backtrack::t3::transitions
-    fst::snd::states, transitions'
+    let acceptLoop = {Start = fst; End = fst; Accepts = Anything}
+    let {Accepts = exitWord} = transitions |> findTransition fst snd
+    let transitions' = extendWithBacktracking fst snd exitWord (snd::states) transitions
+    fst::snd::states, acceptLoop::transitions'
 
 let private parseWord states transitions word =
     match word with
