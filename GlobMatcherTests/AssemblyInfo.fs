557b3760950a7646fa97a74b8a62b740ff5c3881
bert2 <shuairan@gmail.com>
2017-10-31 13:50:11 +0100

    Add some simple tests.

---
+++ GlobMatcherTests/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace GlobMatcherTests.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("GlobMatcherTests")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("GlobMatcherTests")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("a25b1e45-f9ec-412f-a2d3-eb5613faee98")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ GlobMatcherTests/SimpleTests.fs
@@ -0,0 +1,53 @@
+﻿module SimpleTests
+
+open Xunit
+open Parser
+open Acceptor
+
+[<Fact>]
+let ``pattern "a" accepts "a" character`` () =
+    let start,transitions = toAcceptor "a"
+    let result = accept start transitions "a"
+    Assert.True(result)
+
+[<Fact>]
+let ``pattern "a" rejects "b" character`` () =
+    let start,transitions = toAcceptor "a"
+    let result = accept start transitions "b"
+    Assert.False(result)
+
+[<Fact>]
+let ``pattern "a" rejects no character`` () =
+    let start,transitions = toAcceptor "a"
+    let result = accept start transitions ""
+    Assert.False(result)
+
+[<Fact>]
+let ``pattern "?" accepts any character`` () =
+    let start,transitions = toAcceptor "?"
+    let result = accept start transitions "a"
+    Assert.True(result)
+
+[<Fact>]
+let ``pattern "?" rejects no character`` () =
+    let start,transitions = toAcceptor "?"
+    let result = accept start transitions ""
+    Assert.False(result)
+
+[<Fact>]
+let ``pattern "*" accepts any character`` () =
+    let start,transitions = toAcceptor "*"
+    let result = accept start transitions "a"
+    Assert.True(result)
+
+[<Fact>]
+let ``pattern "*" accepts any string of characters`` () =
+    let start,transitions = toAcceptor "*"
+    let result = accept start transitions "abc"
+    Assert.True(result)
+
+[<Fact>]
+let ``pattern "*" accepts no character`` () =
+    let start,transitions = toAcceptor "*"
+    let result = accept start transitions ""
+    Assert.True(result)
