26f090078560936cfe9d7b89215868e912676114
bert2 <shuairan@gmail.com>
2017-11-02 13:53:49 +0100

    Minor line break changes.

--- GlobMatcher/Program.fs
+++ GlobMatcher/Program.fs
@@ -9,13 +9,10 @@ let isMatch pattern text printGraph =
 [<EntryPoint>]
 let main argv = 
     match argv.Length with
-    | 1 -> 
-        printfn "%A" (isMatch argv.[0] "" false)
-    | 2 -> 
-        printfn "%A" (isMatch argv.[0] argv.[1] false)
+    | 1 -> isMatch argv.[0] "" false |> printfn "%A"
+    | 2 -> isMatch argv.[0] argv.[1] false |> printfn "%A"
     | 3 when argv.[2] = "--printGraph" || argv.[2] = "-p" -> 
-        printfn "%A" (isMatch argv.[0] argv.[1] true)
-    | _ -> 
-        printfn "Usage: GlobMatcher.exe <pattern string> <test string> [--printGraph or -p]"
+        isMatch argv.[0] argv.[1] true |> printfn "%A"
+    | _ -> printfn "Usage: GlobMatcher.exe <pattern string> <test string> [--printGraph or -p]"
 
     0 // return an integer exit code
