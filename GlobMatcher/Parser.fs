a4c7ec2504df8c1e8b2c9d7870c168fd1571e771
bert2 <shuairan@gmail.com>
2017-11-02 15:11:49 +0100

    Add tests for pattern sanitization. Make generation of unique state ids testable.

--- GlobMatcher/Parser.fs
+++ GlobMatcher/Parser.fs
@@ -11,8 +11,8 @@ module Parser =
     let private acceptsAnything state =
         List.exists (fun {Start = s; Accepts = w} -> s = state && w = Anything)
 
-    let private extentWithFixedLengthWord (oldStart::states) transitions word =
-        let newStart = State (shortId () |> UniqueId)
+    let private extentWithFixedLengthWord idGenerator (oldStart::states) transitions word =
+        let newStart = State (idGenerator () |> UniqueId)
         let t = {Start = newStart; End = oldStart; Accepts = word}
         newStart::oldStart::states, t::transitions
 
@@ -34,18 +34,18 @@ module Parser =
         let transitions' = extendWithBacktracking fst snd exitWord (snd::states) transitions
         fst::snd::states, acceptLoop::transitions'
 
-    let private parseWord states transitions word =
+    let private parseWord idGenerator states transitions word =
         match word with
         | "*" -> extentWithVariableLengthWord states transitions
-        | "?" -> extentWithFixedLengthWord states transitions Anything
-        | _ -> extentWithFixedLengthWord states transitions (Word word)
+        | "?" -> extentWithFixedLengthWord idGenerator states transitions Anything
+        | _ -> extentWithFixedLengthWord idGenerator states transitions (Word word)
     
-    let rec private parsePattern (start::states) transitions (pattern:string) =
+    let rec private parsePattern idGenerator (start::states) transitions (pattern:string) =
         match pattern.Length with
         | 0 -> start::states, transitions
         | _ -> 
-            let states', transistions' = parseWord (start::states) transitions pattern.[..0]
-            parsePattern states' transistions' pattern.[1..]
+            let states', transistions' = parseWord idGenerator (start::states) transitions pattern.[..0]
+            parsePattern idGenerator states' transistions' pattern.[1..]
 
     let rec private sanitize (pattern:string) =
         if pattern.Contains("**") then
@@ -55,9 +55,11 @@ module Parser =
         else
             pattern
 
-    let toAcceptor pattern =
-        let initial = State (shortId () |> UniqueId)
+    let toAcceptorWithStateId idGenerator pattern =
+        let initial = State (idGenerator () |> UniqueId)
         let halt = Success
         let eof = {Start = initial; End = halt; Accepts = Word ""}
-        parsePattern [initial; halt] [eof] (pattern |> sanitize |> reverse)
+        parsePattern idGenerator [initial; halt] [eof] (pattern |> sanitize |> reverse)
+
+    let toAcceptor = toAcceptorWithStateId shortId 
 
