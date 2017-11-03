d742b1e87299eddde98d8765a7e6243df54a0100
bert2 <shuairan@gmail.com>
2017-11-03 01:32:58 +0100

    Shorten namespace of test project.

--- GlobMatcherTests/AssemblyInfo.fs
+++ GlobMatcherTests/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace GlobMatcherTests.AssemblyInfo
+﻿namespace Tests.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("GlobMatcherTests")>]
+[<assembly: AssemblyTitle("Tests")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("GlobMatcherTests")>]
+[<assembly: AssemblyProduct("Tests")>]
 [<assembly: AssemblyCopyright("Copyright ©  2017")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
