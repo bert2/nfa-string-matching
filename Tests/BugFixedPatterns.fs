a45cdb8d46e48b17d8abfe456a8689ea6e43f7f6
bert2 <shuairan@gmail.com>
2017-11-05 14:16:13 +0100

    Add test for patterns with multiple ? after a *

--- Tests/BugFixedPatterns.fs
+++ Tests/BugFixedPatterns.fs
@@ -25,3 +25,15 @@ let ``should handle ambiguities due to patterns like "*b?"`` text =
     let start::_,transitions = Parser.toAcceptor "*b?"
     let result = Acceptor.run start transitions text
     Assert.True(result)
+
+[<Theory>]
+[<InlineData("bcac")>]
+[<InlineData("cbcac")>]
+[<InlineData("cbbbaaa")>]
+[<InlineData("cbcabcac")>]
+[<InlineData("cbcaccbcac")>]
+[<InlineData("cbacccbcac")>]
+let ``should handle multiple "?" after a "*"`` text =
+    let start::_,transitions = Parser.toAcceptor "*b?a?"
+    let result = Acceptor.run start transitions text
+    Assert.True(result)
