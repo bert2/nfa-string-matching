b19f30f20a72d710bd5de0d57d645a099da12bb0
bert2 <shuairan@gmail.com>
2017-11-13 18:27:36 +0100

    Implement parser for glob patterns.

---
+++ GlobMatcher/GlobParser.fs
@@ -0,0 +1,16 @@
+﻿namespace GlobMatcher
+
+module GlobParser = 
+
+    open AutomatonBuilder
+    
+    let private parse c =
+        match c with
+        | '?' -> makeAnyChar ()
+        | '*' -> makeAnyString ()
+        | c -> makeChar c
+
+    let toAutomaton (pattern:string) =
+        pattern
+        |> Seq.map parse
+        |> Seq.fold concat (makeEmpty ())
