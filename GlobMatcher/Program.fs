cd41dea20231df79d1fc337c82303cc5466ef208
bert2 <shuairan@gmail.com>
2017-11-16 20:11:22 +0100

    Overhaul argument parsing of CLI.

--- GlobMatcher/Program.fs
+++ GlobMatcher/Program.fs
@@ -5,18 +5,42 @@ let printGravizoLink automaton =
     let dotscript = AutomatonPrinter.toDot automaton
     printfn "%s%s" "https://g.gravizo.com/svg?" (Uri.EscapeDataString dotscript)
 
+let gatherUserInputs () =
+    printf "Glob pattern: "
+    let pattern = Console.ReadLine()
+    printf "Text: "
+    let text = Console.ReadLine()
+    printf "Print graph of automaton? (y/n) "
+    let print = Console.ReadKey(true).KeyChar = 'y'
+    printfn ""
+    (pattern, text, print)
+
 let isMatch pattern text printGraph =
-    let M = GlobParser.toAutomaton pattern
-    if printGraph then printGravizoLink M
-    Automaton.run M text
+    let a = GlobParser.toAutomaton pattern
+    if printGraph then printGravizoLink a
+    Automaton.run a text
+
+let exit code =
+#if DEBUG
+    System.Console.ReadKey(true) |> ignore
+#endif
+    code
+
+let getInputs (args:string[]) =
+    match args.Length with
+    | 1 when args.[0] = "--interactive" || args.[0] = "-i" -> 
+        gatherUserInputs ()
+    | 2 -> (args.[0], args.[1], false)
+    | 3 when args.[2] = "--printGraph" || args.[2] = "-p" -> 
+        (args.[0], args.[1], true)
+    | _ -> 
+        printfn "Usage: GlobMatcher.exe <pattern string> <test string> [--printGraph or -p]"
+        printfn "       GlobMatcher.exe [--interactive or -i]"
+        gatherUserInputs ()
 
 [<EntryPoint>]
 let main argv = 
-    match argv.Length with
-    | 1 -> isMatch argv.[0] "" false |> printfn "%A"
-    | 2 -> isMatch argv.[0] argv.[1] false |> printfn "%A"
-    | 3 when argv.[2] = "--printGraph" || argv.[2] = "-p" -> 
-        isMatch argv.[0] argv.[1] true |> printfn "%A"
-    | _ -> printfn "Usage: GlobMatcher.exe <pattern string> <test string> [--printGraph or -p]"
-
-    0 // return an integer exit code
+    let (pattern, text, printGraph) = getInputs argv
+    let result = isMatch pattern text printGraph
+    printfn "Match: %A" result
+    exit (if result then 0 else 1)
