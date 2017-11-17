d224387fc4ae903ef8dc14d9a3039f8404af1058
bert2 <shuairan@gmail.com>
2017-11-17 19:25:42 +0100

    Return syntax errors when parsing character ranges.

--- GlobMatcher/GlobParser.fs
+++ GlobMatcher/GlobParser.fs
@@ -7,7 +7,18 @@ module GlobParser =
     open AutomatonBuilder
     
     let private parseRange (pattern:string) =
-        Success (makeRange pattern.[0] pattern.[2]),  pattern.[4..]
+        match pattern.Length with
+        | 0 -> Failure "Unexpected end of pattern string after start of character range.", pattern
+        | 1 -> Failure "Unexpected end of pattern string after lower boundary of character range.", pattern.[1..]
+        | 2 -> 
+            if pattern.[1] = '-' then
+                Failure "Unexpected end of pattern string after minus sign '-' in character range.", pattern.[2..]
+            else
+                Failure "Expected minus sign '-' to separate lower and upper boundary in character range.", pattern.[2..]
+        | 3 -> Failure "Unexpected end of pattern string after upper boundary of character range.", pattern.[3..]
+        | 4 when pattern.[3] <> ']' -> 
+            Failure "Expected closing bracket ']' at end of character range.", pattern.[4..]
+        | _ -> Success (makeRange pattern.[0] pattern.[2]),  pattern.[4..]
 
     let private parseEscaped (pattern:string) =
         match pattern.Length with
