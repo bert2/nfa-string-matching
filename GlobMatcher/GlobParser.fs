5dada46a212ad0ba2fc0191d123921e7234fde13
bert2 <shuairan@gmail.com>
2017-11-17 20:16:47 +0100

    Propagate unparsed pattern remainder via success case.

--- GlobMatcher/GlobParser.fs
+++ GlobMatcher/GlobParser.fs
@@ -8,44 +8,48 @@ module GlobParser =
     
     let private checkRangeSyntax (pattern:string) =
         match pattern.Length with
-        | 0 -> Failure ("Unexpected end of pattern string after start of character range.", pattern)
-        | 1 -> Failure ("Unexpected end of pattern string after lower boundary of character range.", pattern.[1..])
+        | 0 -> Failure "Unexpected end of pattern string after start of character range."
+        | 1 -> Failure "Unexpected end of pattern string after lower boundary of character range."
         | 2 -> 
             if pattern.[1] = '-' then
-                Failure ("Unexpected end of pattern string after minus sign '-' in character range.", pattern.[2..])
+                Failure "Unexpected end of pattern string after minus sign '-' in character range."
             else
-                Failure ("Expected minus sign '-' to separate lower and upper boundary in character range.", pattern.[2..])
-        | 3 -> Failure ("Unexpected end of pattern string after upper boundary of character range.", pattern.[3..])
+                Failure "Expected minus sign '-' to separate lower and upper boundary in character range."
+        | 3 -> Failure "Unexpected end of pattern string after upper boundary of character range."
         | 4 when pattern.[3] <> ']' -> 
-            Failure ("Expected closing bracket ']' at end of character range.", pattern.[4..])
+            Failure "Expected closing bracket ']' at end of character range."
         | _ -> Success pattern
 
     let private parseRange validatedPattern =
         match validatedPattern with
-        | Success (pattern:string) -> Success (makeRange pattern.[0] pattern.[2]), pattern.[4..]
-        | Failure (err, pattern) -> Failure err, pattern
+        | Success (pattern:string) -> Success (makeRange pattern.[0] pattern.[2], pattern.[4..])
+        | Failure err -> Failure err
 
-    let private parseEscaped (pattern:string) =
+    let private checkEscapeSyntax (pattern:string) =
         match pattern.Length with
-        | 0 -> Failure "Unexpected end of pattern string after escape character '\\'.", pattern
-        | _ -> Success (makeChar pattern.[0]), pattern.[1..]
+        | 0 -> Failure "Unexpected end of pattern string after escape character '\\'."
+        | _ -> Success pattern
+
+    let private parseEscaped validatedPattern =
+        match validatedPattern with
+        | Success (pattern:string) -> Success (makeChar pattern.[0], pattern.[1..])
+        | Failure err -> Failure err
 
     let private parse (pattern:string) =
         match pattern.[0] with
-        | '?'  -> Success (makeAnyChar ()), pattern.[1..]
-        | '*'  -> Success (makeAnyString ()), pattern.[1..]
+        | '?'  -> Success (makeAnyChar (), pattern.[1..])
+        | '*'  -> Success (makeAnyString (), pattern.[1..])
         | '['  -> pattern.[1..] |> checkRangeSyntax |> parseRange
-        | '\\' -> parseEscaped pattern.[1..]
-        | c    -> Success (makeChar c), pattern.[1..]
+        | '\\' -> pattern.[1..] |> checkEscapeSyntax |> parseEscaped
+        | c    -> Success (makeChar c, pattern.[1..])
 
     let toAutomaton pattern =
         let rec toAutomaton' (pattern:string) automaton =
             if pattern.Length = 0 then
                 Success automaton
             else
-                let result, pattern' = parse pattern
-                match result with
-                | Success a -> concat automaton a |> toAutomaton' pattern'
+                match parse pattern with
+                | Success (a, pattern') -> concat automaton a |> toAutomaton' pattern'
                 | Failure err -> Failure ("Syntax error: " + err)
         toAutomaton' pattern (makeEmpty ())
 
