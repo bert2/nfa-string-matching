2ceda4ad1221270620afb02fcbda8bc78fa540db
bert2 <shuairan@gmail.com>
2017-12-10 15:25:18 +0100

    Move performance test code to entry point for now; update fsharp packages.

--- PerformanceTest/Program.fs
+++ PerformanceTest/Program.fs
@@ -1,7 +1,56 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System
+open System.Diagnostics
+open MathNet.Numerics
+open GlobMatcher
+
+let toFloats = List.map float >> List.toArray
+
+let delay f x = fun () -> f x
+
+let makeTestData wildcard word length =
+    let text = String.replicate length word
+    let pattern = (String.replicate length wildcard) + text
+    pattern, text
+
+let measure f = 
+    let timer = Stopwatch.StartNew ()
+    f () |> ignore
+    timer.ElapsedMilliseconds
+
+let measureTimeForPatternLength n = 
+    let pattern, text = makeTestData "*" "a" n
+    let automaton = GlobParser.toAutomaton' pattern
+    measure (fun () -> Automaton.run automaton text)
+
+let repeat n f =
+    [0..n]
+    |> List.map (fun _ -> f ())
+    |> List.map float
+    |> List.average
+
+let polynomial n (ps:float[]) x = 
+    [0..n] 
+    |> List.map (fun order -> ps.[order] * Math.Pow (x, float order))
+    |> List.sum
+
+let fitPolynomial xs ys order =
+    let ps = Fit.polynomial order xs ys
+    let ys' = xs |> Array.map (polynomial order ps)
+    GoodnessOfFit.RSquared (ys', ys)
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    measureTimeForPatternLength 50 |> ignore
+    let lengths = [1..50]
+
+    let durations = 
+        lengths 
+        |> List.map (delay measureTimeForPatternLength)
+        |> List.map (repeat 5)
+
+    let goodness = [1..4] |> List.map (fitPolynomial (toFloats lengths) (List.toArray durations))
+
+    printfn "%A" goodness
+
+    if Debugger.IsAttached then Console.ReadKey(true) |> ignore
+    0
