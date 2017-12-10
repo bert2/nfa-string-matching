f9242d5ddfd487cdccf5b26f3289951fcfff9c65
bert2 <shuairan@gmail.com>
2017-12-10 16:09:23 +0100

    Output performance test progress.

--- PerformanceTest/Program.fs
+++ PerformanceTest/Program.fs
@@ -18,13 +18,17 @@ let measure f =
     timer.ElapsedMilliseconds
 
 let measureTimeForPatternLength n = 
+    printf "\b\b\b\b\b\b\b\b\b%03i (" n
     let pattern, text = makeTestData "*" "a" n
     let automaton = GlobParser.toAutomaton' pattern
     measure (fun () -> Automaton.run automaton text)
 
 let repeat n f =
-    [0..n]
-    |> List.map (fun _ -> f ())
+    [1..n]
+    |> List.map (fun n -> 
+        let v = f ()
+        printf "%02ix)" n
+        v)
     |> List.map float
     |> List.average
 
@@ -38,19 +42,29 @@ let fitPolynomial xs ys order =
     let ys' = xs |> Array.map (polynomial order ps)
     GoodnessOfFit.RSquared (ys', ys)
 
+let doWarmup n =
+    printf "Warming up with pattern of length %i..." n
+    let pattern, text = makeTestData "*" "a" n
+    let automaton = GlobParser.toAutomaton' pattern
+    Automaton.run automaton text |> ignore
+    printfn "done"
+
 [<EntryPoint>]
 let main argv = 
-    measureTimeForPatternLength 50 |> ignore
-    let lengths = [1..50]
+    doWarmup 50
 
+    printf "Running automaton with pattern of length 000 (00x)"
+    let lengths = [1..40]
     let durations = 
         lengths 
         |> List.map (delay measureTimeForPatternLength)
         |> List.map (repeat 5)
+    printfn ""
 
+    printf "Analyzing runtime behaviour..."
     let goodness = [1..4] |> List.map (fitPolynomial (toFloats lengths) (List.toArray durations))
-
+    printfn "done"
     printfn "%A" goodness
 
-    if Debugger.IsAttached then Console.ReadKey(true) |> ignore
+    if Debugger.IsAttached then Console.ReadKey (true) |> ignore
     0
