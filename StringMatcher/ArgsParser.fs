cbe01dde601b1182fc9589a3b4d940c5104d7fcd
bert2 <shuairan@gmail.com>
2018-01-06 17:06:40 +0100

    Add cli argument parser.

---
+++ StringMatcher/ArgsParser.fs
@@ -0,0 +1,63 @@
+﻿namespace StringMatcher
+
+module ArgsParser =
+
+    open System
+    open FParsec
+    open Util
+
+    type Arg = ArgValue of string * string | ArgSwitch of string
+
+    // helpers
+
+    let private isWordChar c = Char.IsLetterOrDigit c || c = '-' || c = '_'
+
+    // atomics
+
+    let private letter = satisfy Char.IsLetter
+
+    let private wordchar = satisfy isWordChar
+
+    let private noneSpace = satisfy (not << Char.IsWhiteSpace)
+
+    let private noneQuote = satisfy (not << (=) '\'')
+
+    let private noneDoubleQuote = satisfy (not << (=) '"')
+
+    let private noneDash = satisfy (not << (=) '-')
+
+    let private identifier = letter .>>. many1 wordchar |>> (List.Cons >> charsToStr)
+
+    // argument keys
+
+    let private longArgkey = skipString "--" >>. identifier <?> "--argumentKey"
+
+    let private shortArgkey = skipChar '-' >>. letter |>> string <?> "argument shorthand (-a)"
+
+    let private argkey = longArgkey <|> shortArgkey
+
+    // argument values
+
+    let private unquotedArgvalue = many1Chars2 noneDash noneSpace
+
+    let private quotedArgvalue = skipChar '\'' >>. manyChars noneQuote .>> skipChar '\''
+
+    let private doubleQuotedArgvalue = skipChar '"' >>. manyChars noneDoubleQuote .>> skipChar '"'
+
+    let private argvalue = quotedArgvalue <|> doubleQuotedArgvalue <|> unquotedArgvalue
+
+    // argument list
+
+    let private argsep = spaces1 <|> eof
+
+    let private arg = argkey .>> spaces1 .>>. argvalue .>> argsep
+
+    let private switch = argkey .>> argsep |>> fun k -> (k, "")
+
+    let private parser = many (attempt arg <|> switch) .>> eof
+
+    let parseArgs argv = 
+        let result = String.concat " " argv |> run parser
+        match result with
+        | Failure (msg, _, _)  -> Result.Failure msg
+        | Success (args, _, _) -> Result.Success <| Map.fromPairs args
--- StringMatcher/Util.fs
+++ StringMatcher/Util.fs
@@ -8,6 +8,14 @@ let globalCount =
         i <- i + 1
         i
 
+let charsToStr = List.toArray >> System.String >> string
+
+module Map =
+
+    let fromPairs pairs = 
+        let add m (k, v) = Map.add k v m
+        Seq.fold add Map.empty pairs
+
 module List =
 
     let inline foldBack' folder state list = List.foldBack folder list state
