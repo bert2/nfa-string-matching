e2255b201cf3b1fedc2cd410b1e2459e7a7cc61c
bert2 <shuairan@gmail.com>
2018-01-21 13:57:43 +0100

    Add regex support to string matcher executable.

--- StringMatcher/Program.fs
+++ StringMatcher/Program.fs
@@ -5,23 +5,26 @@ open StringMatcher
 open Result
 
 type Args =
-    | [<AltCommandLine("-p")>][<Unique>][<Mandatory>]
-        Pattern of string
-    | [<AltCommandLine("-t")>][<Unique>][<Mandatory>] 
+    | [<AltCommandLine("-g")>][<Unique>]
+        Glob of string
+    | [<AltCommandLine("-r")>][<Unique>]
+        Regex of string
+    | [<AltCommandLine("-t")>][<Unique>] 
         Text of string
-    | [<AltCommandLine("-g")>] 
+    | [<AltCommandLine("-p")>] 
         PrintGraph
     | [<AltCommandLine("-i")>] 
         Interactive
     interface IArgParserTemplate with
         member this.Usage: string = 
             match this with
-            | Pattern _   -> "the pattern describing the strings to match"
+            | Glob _      -> "the glob pattern describing the strings to match (not combinable with '--regex')"
+            | Regex _     -> "the regex pattern describing the strings to match (not combinable with '--glob')"
             | Text _      -> "the string to be matched"
             | PrintGraph  -> "prints the graph of the generated NFA as a link to https://gravizo.com/."
             | Interactive -> "starts interactive mode"
 
-let argsParser = ArgumentParser.Create<Args> ()
+let argsParser = ArgumentParser.Create<Args> (errorHandler = new ProcessExiter ())
 
 let exit exitcode =
     if Debugger.IsAttached then Console.ReadKey(true) |> ignore
@@ -34,18 +37,24 @@ let printGravizoLink automaton =
 let gatherUserInputs () =
     printf "Pattern: "
     let pattern = Console.ReadLine()
-    printf "Text: "
+    printfn "Pattern uses glob syntax? Otherwise regex will be assumed. (y/n)"
+    let parseF = if Console.ReadKey(true).KeyChar = 'y' then GlobParser.toAutomaton else RegexParser.toAutomaton
+    printf "Text to match: "
     let text = Console.ReadLine()
     printf "Print graph of automaton? (y/n) "
     let print = Console.ReadKey(true).KeyChar = 'y'
     printfn ""
-    (pattern, text, print)
+    (pattern, parseF, text, print)
 
 let getInputs (args:ParseResults<Args>) =
     if args.Contains <@ Interactive @> then
         gatherUserInputs ()
     else
-        (args.GetResult <@ Pattern @>, args.GetResult <@ Text @>, args.Contains <@ PrintGraph @>)
+        let (pattern, parseF) =
+            match args.TryGetResult <@ Glob @> with
+            | Some p -> p, GlobParser.toAutomaton
+            | _      -> args.GetResult <@ Regex @>, RegexParser.toAutomaton
+        (pattern, parseF, args.GetResult <@ Text @>, args.Contains <@ PrintGraph @>)
 
 [<EntryPoint>]
 let main argv = 
@@ -53,9 +62,17 @@ let main argv =
     if args.IsUsageRequested then 
         argsParser.PrintUsage () |> printfn "%s"
         2
+    elif not (args.Contains <@ Regex @>) && not (args.Contains <@ Glob @>) then
+        printfn "ERROR:  missing parameter '--glob' or '--regex'."
+        argsParser.PrintUsage () |> printfn "%s"
+        2
+    elif args.Contains <@ Regex @> && args.Contains <@ Glob @> then
+        printfn "ERROR:  parameter '--glob' cannot be combined with '--regex'."
+        argsParser.PrintUsage () |> printfn "%s"
+        2
     else
-        let (pattern, text, printGraph) = getInputs args
-        match GlobParser.toAutomaton pattern with
+        let (pattern, parseF, text, printGraph) = getInputs args
+        match parseF pattern with
         | Failure msg -> 
             printfn "Incorrect pattern:\n%s" msg
             3
