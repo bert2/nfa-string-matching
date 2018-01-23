d77facbcadbb649e69657e935f961ec86fd4fb04
bert2 <shuairan@gmail.com>
2018-01-23 21:06:18 +0100

    Add first attempt of property test for automaton builder.

---
+++ Tests/ProtoAutomaton.fs
@@ -0,0 +1,41 @@
+﻿module ProtoAutomaton
+
+open Xunit
+open FsCheck
+open StringMatcher
+open AutomatonBuilder
+
+let rec protoAutomaton = 
+    let leafProtos = [
+        Gen.map makeChar    Arb.generate<char>
+        Gen.map makeAnyChar (Gen.constant ())
+        Gen.map makeRange   Arb.generate<char*char>]
+
+    let rec protoAutomaton' size =
+        if size <= 0 then
+            Gen.oneof leafProtos
+        else
+            let subProto = protoAutomaton' (size - 1)
+            let nestedProtos = [
+                Gen.map  makeZeroOrMore  subProto
+                Gen.map  makeOneOrMore   subProto
+                Gen.map  makeZeroOrOne   subProto
+                Gen.map2 makeAlternation subProto subProto]
+            Gen.oneof <| leafProtos @ nestedProtos
+
+    Gen.sized protoAutomaton'
+
+[<Fact>]
+let ``has identity element`` () =
+    Check.VerboseThrowOnFailure (Prop.forAll 
+        (Arb.fromGen protoAutomaton)
+        (fun proto -> 
+            let proto' = connect proto empty
+
+            resetIdGenerator ()
+            let a = complete proto Final
+
+            resetIdGenerator ()
+            let a' = complete proto' Final
+
+            a'.ToString() = a.ToString()))
