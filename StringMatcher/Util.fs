767296a22617935095c8e284e48dc2b5f514c123
bert2 <shuairan@gmail.com>
2018-01-24 19:43:00 +0100

    Remove unused functions.

--- StringMatcher/Util.fs
+++ StringMatcher/Util.fs
@@ -2,20 +2,6 @@
 
 let sortTuple (l, r) = (min l r, max l r)
 
-let makeGlobalCounter () = 
-    let mutable i = -1
-    fun () ->
-        i <- i + 1
-        i
-
-let charsToStr = List.toArray >> System.String >> string
-
-module Map =
-
-    let fromPairs pairs = 
-        let add m (k, v) = Map.add k v m
-        Seq.fold add Map.empty pairs
-
 module List =
 
     let inline foldBack' folder state list = List.foldBack folder list state
