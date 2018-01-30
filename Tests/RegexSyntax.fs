f9bc0c6f4dc6dfc447a6e1e75cbd0ddd4257c8cb
bert2 <shuairan@gmail.com>
2018-01-30 22:56:51 +0100

    Extend test of alternation precedence.

--- Tests/RegexSyntax.fs
+++ Tests/RegexSyntax.fs
@@ -97,7 +97,12 @@ let ``Alternation binds weaker than concatenation`` p t r = assertMatch p t r
 [<InlineData("a|b*", "bb", true)>]
 [<InlineData("a|b*", "aa", false)>]
 [<InlineData("a|b*", "ab", false)>]
-let ``Alternation binds weaker than Kleene star`` p t r = assertMatch p t r
+[<InlineData("a|b+", "a", true)>]
+[<InlineData("a|b+", "b", true)>]
+[<InlineData("a|b+", "bb", true)>]
+[<InlineData("a|b+", "aa", false)>]
+[<InlineData("a|b+", "ab", false)>]
+let ``Alternation binds weaker than repetitions`` p t r = assertMatch p t r
 
 [<Theory>]
 [<InlineData("a*b", "b", true)>]
