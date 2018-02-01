b1d1a1b02c48b8c5bf2dd117c19a2390857fb001
bert2 <shuairan@gmail.com>
2018-02-01 20:37:55 +0100

    Improve performance by reducing number of list concats and using sets.

--- StringMatcher/Autom.fs
+++ StringMatcher/Autom.fs
@@ -1,18 +1,20 @@
 ﻿namespace StringMatcher
 
-type Letter = 
-    | Letter of char
-    | Range of char * char 
-    | Any
+type Letter = Any | Letter of char | Range of char * char 
 
-[<ReferenceEquality>]
+[<CustomEquality; CustomComparison>]
 type State = 
     | State of Letter * State
     | Split of State * State
     | Final
+    override x.Equals y = System.Object.ReferenceEquals (x, y)
+    interface System.IComparable with
+        member x.CompareTo yobj =
+            match yobj with
+            | :? State as y -> compare (x.GetHashCode ()) (y.GetHashCode ())
+            | _ -> invalidArg "yobj" "cannot compare value of different types"
 
 module Autom =
-    open System.Collections.Generic
 
     let rec private moveNext letter state =
         match state, letter with
@@ -22,27 +24,25 @@ module Autom =
         | State (Range (min, max), next), Letter c 
             when min <= c && c <= max              -> Some next
         | _                                        -> None
-
-    let private skipEpsilons state =
-        let visited = HashSet<_> ()
-        let rec skipEpsilons' state = 
-            if visited.Contains state then 
-                [] 
-            else 
-                match state with
-                | Split (left, right) -> 
-                    visited.Add state |> ignore
-                    skipEpsilons' left @ skipEpsilons' right
-                | state -> [state]
-        skipEpsilons' state
     
+    let rec private skipEpsilons visited included toInclude =
+        match toInclude with
+        | [] -> included
+        | state::toInclude' ->
+            if visited |> Set.contains state then
+                skipEpsilons visited included toInclude'
+            else
+                match state with
+                | Split (l, r) -> skipEpsilons (visited |> Set.add state) included (l::r::toInclude')
+                | _ -> skipEpsilons visited (included |> Set.add state) toInclude'
+
     let private consume currents letter =
        currents 
+       |> Set.toList
        |> List.choose (moveNext letter) 
-       |> List.collect skipEpsilons
-       |> List.distinct
+       |> skipEpsilons Set.empty Set.empty
 
     let run start = 
         Seq.map Letter 
-        >> Seq.fold consume (skipEpsilons start)
-        >> List.contains Final
+        >> Seq.fold consume (skipEpsilons Set.empty Set.empty (List.singleton start))
+        >> Set.contains Final
